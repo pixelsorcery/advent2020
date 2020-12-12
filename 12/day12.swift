2cd133c4332c62ca82ac57a1186c815ee76413c9
pixelsorcery <4041512+pixelsorcery@users.noreply.github.com>
2020-12-11 22:13:42 -0800

    day 12

---
+++ 12/day12.swift
@@ -0,0 +1,92 @@
+import Foundation
+
+let path = Bundle.main.path(forResource: "input", ofType: "txt")!
+let text = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
+var lines = text.components(separatedBy: "\n")
+
+let N: (x:Int, y:Int) = (0, 1)
+let E: (x:Int, y:Int) = (1, 0)
+let S: (x:Int, y:Int) = (0, -1)
+let W: (x:Int, y:Int) = (-1, 0)
+
+let dirs = [E, S, W, N]
+
+var curDir = 0
+var curPos: (x:Int, y:Int) = (0,0)
+
+
+for line in lines {
+    let amt = Int(line.filter("1234567890".contains))!
+    let dir = line.prefix(1)
+    switch(dir) {
+        case "F":
+            curPos.x += dirs[curDir].x * amt
+            curPos.y += dirs[curDir].y * amt
+        case "E":
+            curPos.x += E.x * amt
+            curPos.y += E.y * amt
+        case "S":
+            curPos.x += S.x * amt
+            curPos.y += S.y * amt
+        case "W":
+            curPos.x += W.x * amt
+            curPos.y += W.y * amt
+        case "N":
+            curPos.x += N.x * amt
+            curPos.y += N.y * amt
+        case "R":
+            curDir = (curDir + (amt/90)) % 4
+        case "L":
+            curDir = ((curDir - (amt/90)) + 4) % 4
+        default:
+            break
+    }
+}
+
+print (curPos.x)
+print (curPos.y)
+print ("part 1: \(abs(curPos.x) + abs(curPos.y))")
+
+curPos = (0, 0)
+var wayPt: (x:Int, y:Int) = (10, 1)
+
+for line in lines {
+    let amt = Int(line.filter("1234567890".contains))!
+    let dir = line.prefix(1)
+
+    switch(dir) {
+        case "F":
+            curPos.x += wayPt.x * amt
+            curPos.y += wayPt.y * amt
+        case "E":
+            wayPt.x += E.x * amt
+            wayPt.y += E.y * amt
+        case "S":
+            wayPt.x += S.x * amt
+            wayPt.y += S.y * amt
+        case "W":
+            wayPt.x += W.x * amt
+            wayPt.y += W.y * amt
+        case "N":
+            wayPt.x += N.x * amt
+            wayPt.y += N.y * amt
+        case "R":
+            let rot = amt/90
+            for _ in 0..<rot {
+                let tmp = wayPt.x * -1
+                wayPt.x = wayPt.y
+                wayPt.y = tmp
+            }
+        case "L":
+            let rot = amt/90
+            for _ in 0..<rot {
+                let tmp = wayPt.y * -1
+                wayPt.y = wayPt.x
+                wayPt.x = tmp
+            }
+        default:
+            break
+    }
+}
+
+print ("part 2: \(abs(curPos.x) + abs(curPos.y))")
