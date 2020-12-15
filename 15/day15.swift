f0920c2df351d39874ab93112241414182aa373f
pixelsorcery <4041512+pixelsorcery@users.noreply.github.com>
2020-12-14 22:24:14 -0800

    revert back to easier to read version

--- 15/day15.swift
+++ 15/day15.swift
@@ -3,26 +3,33 @@ import Foundation
 let input = [7,12,1,0,16,2]
 var prevNum:[Int: [Int]] = [:]
 
+func appendTurn(_ nums: inout [Int], _ val: Int) {
+    switch (nums.count) {
+        case 0, 1:
+            nums.append(val)
+        case 2:
+            nums.removeFirst(1)
+            nums.append(val)
+        default:
+            break
+    }
+}
+
 for i in 0..<input.count {
     prevNum[input[i]] = [i+1]
 }
 
 var lastNum = input.last!
+
 for i in input.count..<30000000 {
     if (prevNum[lastNum]!.count == 1) {
-        if prevNum[0]!.count == 2 {
-            prevNum[0]!.removeFirst(1)
-        }
-        prevNum[0]!.append(i+1)
+        appendTurn(&prevNum[0]!, i+1)
         lastNum = 0
     } else {
         let last2Turns = prevNum[lastNum]!
         lastNum = last2Turns[1] - last2Turns[0]
         if prevNum[lastNum] != nil {
-            if prevNum[lastNum]!.count == 2 {
-                prevNum[lastNum]!.removeFirst(1)
-            }
-            prevNum[lastNum]!.append(i+1)
+            appendTurn(&prevNum[lastNum]!, i+1)
         } else {
             prevNum[lastNum] = [i+1]
         }
